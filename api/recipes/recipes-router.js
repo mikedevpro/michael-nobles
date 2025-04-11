7b3ca721e93c1b57a980af8acb8b8f1ad22c883d
Michael Nobles <Mnobles33@gmail.com>
2025-04-11 11:35:31 -0700

    Next commit

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+router.use('*', (req, res, next) => {
+  res.json({ api: 'up' })
+})
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ 
+    customMessage: 'something went wrong inside the recipers router',
+    message: err.message,
+    stack: err.stack
+  })
+})
+
+
+module.exports = router
